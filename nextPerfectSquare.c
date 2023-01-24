9fec956a250f4d1be05f19f71699ff13979458d3
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-01-24 21:11:28 +0545

    program to find next perfect square

---
+++ nextPerfectSquare.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int nextPerfectSquare(int);
+
+int main()
+{
+    int n;
+    while (1)
+    {
+        printf("\nEnter a number: ");
+        scanf("%d", &n);
+
+        printf("Next perfect square is: %d.\n", nextPerfectSquare(n));
+    }
+    return 0;
+}
+
+int nextPerfectSquare(int n)
+{
+    // find largest perfect square smaller or equal to n
+    // get next perfect square
+    int i;
+    int nextSquare;
+    for (i = 0; i * i <= n; i++)
+    {
+        nextSquare = (i + 1) * (i + 1);
+        if (nextSquare > n)
+        {
+            return nextSquare;
+        }
+    }
+    return 0;
+}
